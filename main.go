a36f0add132a109c633cf104bfb19b425846ed4a
kempedron@gmail.com <kepr@archlinux.localdomain>
2026-02-04 18:19:34 +0300

    3

--- main.go
+++ main.go
@@ -13,20 +13,22 @@ const (
 	TOKEN_INT   TOKEN_TYPE = iota
 	TOKEN_PRINT            // print
 	TOKEN_EOF
-	TOKEN_IDENT
-	TOKEN_SEMICOLON // ;
-	TOKEN_ASSIGN
+	TOKEN_IDENT       // имя переменной
+	TOKEN_SEMICOLON   // ;
+	TOKEN_ASSIGN      // =
 	TOKEN_LPAREN      // (
 	TOKEN_RPAREN      // )
 	TOKEN_INT_KEYWORD // int
 	TOKEN_COMMA       // ,
 	TOKEN_SPACE       // пробел
 	TOKEN_STRING
+	TOKEN_STRING_KEYWORD
 	TOKEN_PLUS
 	TOKEN_MINUS
 	TOKEN_MULTIPLY
 	TOKEN_DIVIDE
 	TOKEN_MODULO
+	TOKEN_QUOTATION
 )
 
 func main() {
@@ -98,6 +100,10 @@ func (l *Lexer) NextToken() Token {
 		if value == "print" {
 			return Token{TOKEN_PRINT, value}
 		}
+
+		if value == "string" {
+			return Token{TOKEN_STRING_KEYWORD, value}
+		}
 		return Token{TOKEN_IDENT, value}
 	}
 
@@ -116,12 +122,15 @@ func (l *Lexer) NextToken() Token {
 		return Token{TOKEN_RPAREN, ")"}
 	case '"':
 		start := l.CurrentPos
+		l.CurrentPos++
+
 		for l.CurrentPos < len(l.InputString) && l.InputString[l.CurrentPos] != '"' {
 			l.CurrentPos++
 		}
-		val := l.InputString[start:l.CurrentPos]
+
+		stringContent := l.InputString[start:l.CurrentPos]
 		l.CurrentPos++
-		return Token{TOKEN_STRING, val}
+		return Token{TOKEN_STRING, stringContent}
 	case ',':
 		return Token{TOKEN_COMMA, ","}
 	case '+':
@@ -134,6 +143,10 @@ func (l *Lexer) NextToken() Token {
 		return Token{TOKEN_DIVIDE, "/"}
 	case '%':
 		return Token{TOKEN_MODULO, "%"}
+	case ' ':
+		fmt.Println("найден пробел")
+		return Token{TOKEN_SPACE, " "}
+
 	}
 
 	panic(fmt.Sprintf("неизвестный символ: %c", ch))
@@ -162,6 +175,9 @@ func (p *Parser) expect(t TOKEN_TYPE) Token {
 }
 
 func (parser *Parser) advanceParser() {
+	if parser.CurrentToken.Type == TOKEN_EOF {
+		os.Exit(0)
+	}
 	parser.CurrentToken = parser.Lexer.NextToken()
 }
 
@@ -170,19 +186,39 @@ func (p *Parser) Parse() {
 	case TOKEN_INT_KEYWORD:
 		p.parseInt()
 	case TOKEN_PRINT:
-		//fmt.Println("printing token..\n")
 		p.printToken()
 	case TOKEN_SEMICOLON:
 		p.advanceParser()
+	case TOKEN_STRING_KEYWORD:
+		p.parseStr()
+	case TOKEN_IDENT:
+		p.parseReAssignVar()
 	default:
 		panic(fmt.Sprintf("неизвестный токен: %v", p.CurrentToken.Type))
 	}
 
 }
 
+func (p *Parser) parseReAssignVar() {
+	varName := p.expect(TOKEN_IDENT)
+	p.expect(TOKEN_ASSIGN)
+	varVal := p.expect(TOKEN_INT)
+	p.setVar(varName.Val, varVal.Val)
+	p.expect(TOKEN_SEMICOLON)
+}
+
 func (p *Parser) printToken() {
 	p.expect(TOKEN_PRINT)
 	p.expect(TOKEN_LPAREN)
+	if p.CurrentToken.Type == TOKEN_QUOTATION {
+		p.printString()
+	} else {
+		p.printVar()
+	}
+
+}
+
+func (p *Parser) printVar() {
 
 	if p.CurrentToken.Type == TOKEN_RPAREN {
 		p.expect(TOKEN_RPAREN)
@@ -201,19 +237,54 @@ func (p *Parser) printToken() {
 		}
 		break
 	}
-
+	p.advanceParser()
 	p.expect(TOKEN_RPAREN)
 	p.expect(TOKEN_SEMICOLON)
 
 	fmt.Println()
+}
 
+func (p *Parser) printString() {
+	p.expect(TOKEN_QUOTATION)
+
+	for {
+		fmt.Println(p.CurrentToken)
+		p.printArgument()
+
+		if p.CurrentToken.Type == TOKEN_COMMA {
+			p.expect(TOKEN_COMMA)
+			fmt.Print(" ")
+			continue
+		}
+		break
+	}
+
+	p.expect(TOKEN_QUOTATION)
+	p.expect(TOKEN_RPAREN)
+	p.expect(TOKEN_SEMICOLON)
+	fmt.Println("закончил выводить строку")
+}
+
+func (p *Parser) isVar(varName string) bool {
+	if _, ok := p.Variables[varName]; ok {
+		return true
+	}
+	return false
 }
 
 func (p *Parser) printArgument() {
-	if p.CurrentToken.Type == TOKEN_STRING {
+	switch p.CurrentToken.Type {
+	case TOKEN_STRING:
+		fmt.Println("это string")
 		token := p.expect(TOKEN_STRING)
 		fmt.Print(token.Val)
-	} else {
+	case TOKEN_IDENT:
+		if !p.isVar(p.CurrentToken.Val) {
+			panic(fmt.Sprintf("undeclarated var: %v", p.CurrentToken.Val))
+		}
+		val := p.Variables[p.CurrentToken.Val]
+		fmt.Print(val)
+	case TOKEN_INT:
 		result := p.parseExpression()
 		fmt.Print(result)
 	}
@@ -227,6 +298,16 @@ func (p *Parser) parseInt() {
 	result := p.parseExpression()
 	p.expect(TOKEN_SEMICOLON)
 	p.setVar(ident.Val, strconv.Itoa(result))
+}
+
+func (p *Parser) parseStr() {
+	p.expect(TOKEN_STRING_KEYWORD)
+	ident := p.expect(TOKEN_IDENT)
+	p.expect(TOKEN_ASSIGN)
+	t := p.expect(TOKEN_STRING)
+
+	p.expect(TOKEN_SEMICOLON)
+	p.setVar(ident.Val, t.Val)
 }
 
 func (p *Parser) setVar(varname string, val string) {
