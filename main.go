206cf9f310345f4435a9e787ced3523dbb9942f5
kempedron@gmail.com <kepr@archlinux.localdomain>
2026-03-09 18:23:23 +0300

    save 18:34

--- main.go
+++ main.go
@@ -13,25 +13,26 @@ const (
 	TOKEN_INT   TOKEN_TYPE = iota
 	TOKEN_PRINT            // print
 	TOKEN_EOF
-	TOKEN_IDENT       // имя переменной
-	TOKEN_SEMICOLON   // ;
-	TOKEN_ASSIGN      // =
-	TOKEN_LPAREN      // (
-	TOKEN_RPAREN      // )
-	TOKEN_INT_KEYWORD // int
-	TOKEN_COMMA       // ,
-	TOKEN_SPACE       // пробел
-	TOKEN_STRING
-	TOKEN_STRING_KEYWORD
-	TOKEN_PLUS
-	TOKEN_MINUS
-	TOKEN_MULTIPLY
-	TOKEN_DIVIDE
-	TOKEN_MODULO
-	TOKEN_QUOTATION
-	TOKEN_FLOAT
-	TOKEN_FLOAT_KEYWORD
-	TOKEN_INPUT_KEYWORD
+	TOKEN_IDENT          // имя переменной
+	TOKEN_SEMICOLON      // ;
+	TOKEN_ASSIGN         // =
+	TOKEN_LPAREN         // (
+	TOKEN_RPAREN         // )
+	TOKEN_INT_KEYWORD    // int
+	TOKEN_COMMA          // ,
+	TOKEN_SPACE          // пробел
+	TOKEN_STRING         // type string
+	TOKEN_STRING_KEYWORD // 'string'
+	TOKEN_PLUS           // +
+	TOKEN_MINUS          // -
+	TOKEN_MULTIPLY       // *
+	TOKEN_DIVIDE         // /
+	TOKEN_MODULO         // %
+	TOKEN_QUOTATION      // "
+	TOKEN_FLOAT          // type float
+	TOKEN_FLOAT_KEYWORD  // 'float'
+	TOKEN_INPUT_KEYWORD  // 'input'
+	TOKEN_NEW_LINE       //  \n
 )
 
 func main() {
